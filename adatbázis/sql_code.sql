b9d16b5c87d21bd7ba5c1ff60c295d74ec3df001
adamcserge <cserge.adam0726@gmail.com>
2025-05-05 09:31:23 +0200

    Index design

--- adatbázis/sql_code.sql
+++ adatbázis/sql_code.sql
@@ -1,4 +1,4 @@
-INSERT INTO szervezet (szervezetNEV)
+/*INSERT INTO szervezet (szervezetNEV)
 VALUES ('Soproni Egyetem');
 
 
@@ -37,31 +37,12 @@ select * from hallgato;
 select * from hallgato where hallgatoID=1;
 SELECT * FROM hallgato;
 
-ALTER TABLE szervezet DROP FOREIGN KEY szervezet_ibfk_1;
-ALTER TABLE szervezet DROP FOREIGN KEY resztvevok_ibfk_1;
 
--- Ezután módosítsd a szervezetID-t
-ALTER TABLE szervezet
-MODIFY COLUMN szervezetID INT AUTO_INCREMENT;
-
--- Majd add vissza az idegen kulcs kapcsolatot
-ALTER TABLE szervezet
-ADD CONSTRAINT szervezet_ibfk_1 FOREIGN KEY (szervezetID) REFERENCES szervezet(szervezetID);
-
--- 1. Típus oszlop hozzáadása INT típusban, alapértelmezett értékkel 0 (nincs meghatározva)
-ALTER TABLE dokumentumok
-ADD COLUMN tipus INT NOT NULL DEFAULT 0;  -- Alapértelmezett 0
-
--- 2. Feltöltés dátuma mező hozzáadása
-ALTER TABLE dokumentumok
-ADD COLUMN feltoltesDatuma TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
 show create table dokumentumok;
 show create table resztvevok;
 select * from dokumentumok;
 
-ALTER TABLE dokumentumok ADD COLUMN eredeti_nev VARCHAR(255);
-ALTER TABLE dokumentumok ADD COLUMN feltolto VARCHAR(255);
 
 INSERT INTO `tema` (
   `temaCim`, 
@@ -89,4 +70,78 @@ CREATE TABLE ertesites (
   datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 show tables;
-show create table resztvevok
+show create table resztvevok;
+ALTER TABLE tema
+ADD COLUMN kulsoKonzulens VARCHAR(255) DEFAULT NULL;
+
+CREATE TABLE kulso_konzulens (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nev VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+
+show create table konzulens;
+ALTER TABLE tema
+ADD COLUMN kulso  DEFAULT NULL;
+
+select * from konzulens;
+select *from tema;
+select * from resztvevok;
+select temaCim from tema where konzulensID=1;
+
+show create table tema;
+
+SELECT rvID, rvNEV AS nev FROM resztvevok WHERE rvID != 1;
+select * from konzulens;
+select *from biralo;
+select * from tema;
+update tema set biralva=0 where temaID=3;
+
+
+UPDATE tema SET biraloID = NULL where temaID =1;
+DELETE FROM biralo
+WHERE BtemaID = 5;
+
+select * from biralo;
+
+INSERT INTO `biralo` (
+  `temaCim`, 
+  `temacsoport`, 
+  `hallgatoID`, 
+  `konzulensID`, 
+  `biraloID`, 
+  `szervezetID`, 
+  `biralva`
+) VALUES (
+  'Próba', 1, 1, 1, 2, 2, 0);
+  
+select * from biralo;
+INSERT INTO resztvevok (rvEmail, rvFelhasznalonev, rvNEV, rvSzervezetID, rvVegzetseg, rvJelszo)
+VALUES ('teszt@example.com', 'tesztfelhasznalo', 'Teszt Név', 1, 1 , 'jelszo123');
+
+show create table biralo;
+
+SELECT t.temaID, t.temaCim
+FROM tema t
+INNER JOIN biralo b ON t.temaID = b.BtemaID
+WHERE b.BbiraloID = 13
+  AND b.allapot IN ('felkeres', 'elfogadva');
+  
+
+
+UPDATE tema
+SET biralva = 0
+WHERE temaID = 11;
+
+select * from biralo;
+show create table biralo;
+
+
+SELECT table_name, constraint_name
+FROM information_schema.key_column_usage
+WHERE referenced_table_name = 'biralo';
+
+ALTER TABLE biralo DROP FOREIGN KEY biralo_ibfk_1;
+ALTER TABLE biralo DROP FOREIGN KEY biralo_ibfk_2;
+
+ALTER TABLE biralo DROP PRIMARY KEY;*/
